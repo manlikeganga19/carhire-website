31dde1b36d5786448e2d75447f2812d422641e27
Daniel <owenjodaniel19@gmail.com>
2023-12-15 15:29:25 +0300

    worked on the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleLogin = () => {
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h4>Login in to your account</h4>
+      <form style={styles.form}>
+        <label style={styles.label}>
+          Username:
+          <input
+            type="text"
+            style={styles.input}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label style={styles.label}>
+          Password:
+          <input
+            type="password"
+            style={styles.input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" style={styles.button} onClick={handleLogin}>
+          Login
+        </button>
+        <p style={styles.registerLink}>
+          Don't have an account? <a href="/sign-up">Register</a>
+        </p>
+      </form>
+
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    width: '300px',
+    margin: 'auto',
+    marginTop: '6em',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: '20px',
+  },
+  label: {
+    marginBottom: '8px',
+  },
+  input: {
+    padding: '8px',
+    marginBottom: '16px',
+    width: '100%',
+    boxSizing: 'border-box',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    padding: '10px 15px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    border: 'none',
+    fontSize: '16px',
+  },
+  registerLink: {
+    marginTop: '10px',
+    fontSize: '14px',
+  },
+};
+
+
+export default Login;
