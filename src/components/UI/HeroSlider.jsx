d9d573f9816883b6f92e460e136b03749d1ea26d
Daniel <owenjodaniel19@gmail.com>
2023-12-15 10:07:09 +0300

    home banner

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -22,8 +22,7 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-01 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
+             <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
 
             <button className="btn reserve__btn mt-4">
               <Link to="/cars">Reserve Now</Link>
@@ -35,7 +34,6 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-02 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
             <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
 
             <button className="btn reserve__btn mt-4">
@@ -48,7 +46,6 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-03 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
             <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
 
             <button className="btn reserve__btn mt-4">
