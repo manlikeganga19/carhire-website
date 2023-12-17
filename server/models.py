eaadb3fe99c1a759527e2b148d865269bf2023e1
Daniel <owenjodaniel19@gmail.com>
2023-12-17 10:33:40 +0300

    db instance created

--- server/models.py
+++ server/models.py
@@ -1,19 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'users'
-    __table_args__ = {'schema': 'rent'}  
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password, method='sha256')
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    # Add other columns as needed
